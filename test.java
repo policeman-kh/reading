57b609c665dde6b5fbc08825823e31191ff970e2
policeman-kh <policeman_kh@msn.com>
2021-11-14 09:25:30 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,21 @@
 public class ArmeriaConfiguration {
     @Bean
-    public MeterRegistry meterRegistry(){
-        return new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    public WebClient webClient(MeterRegistry meterRegistry, HttpTracing tracing) {
+        return WebClient.builder("http://xxxx:8081/")
+                        .decorator(circuitBreakerDecorator(meterRegistry))
+                        .build();
+    }
+
+    private static Function<? super HttpClient, CircuitBreakerClient> circuitBreakerDecorator(
+            MeterRegistry meterRegistry) {
+        final CircuitBreakerRule rule = CircuitBreakerRule.builder()
+                                                          .onServerErrorStatus()
+                                                          .onException()
+                                                          .thenFailure();
+        final CircuitBreaker circuitBreaker =
+                CircuitBreaker.builder("BackendApiClient")
+                              .listener(CircuitBreakerListener.metricCollecting(meterRegistry))
+                              .build();
+        return CircuitBreakerClient.newDecorator(circuitBreaker, rule);
     }
 }
